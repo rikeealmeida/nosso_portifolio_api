13e9fc387ecae6bf3596f5e155a90cd311cce191
unknown <henrique.almeida.ads@gmail.com>
2022-12-27 18:14:55 -0300

    feat: add MongoCreateUserRepository

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import { config } from "dotenv";
 import { MongoGetUsersRepository } from "./repositories/get-users/mongo-get-users";
 import { GetUsersController } from "./controllers/get-users/get-users";
 import { MongoClient } from "./database/mongo";
+import { MongoCreateUserRepository } from "./repositories/create-user/mongo-create-user.";
+import { CreateUserController } from "./controllers/create-user/create-user";
 
 const main = async () => {
   config();
@@ -20,6 +22,18 @@ const main = async () => {
     const getUsersController = new GetUsersController(mongoGetUsersRepository);
     const { body, statusCode } = await getUsersController.handle();
 
+    res.send(body).status(statusCode);
+  });
+
+  app.post("/users", async (req, res) => {
+    const mongoCreateUserRepository = new MongoCreateUserRepository();
+    const createUserController = new CreateUserController(
+      mongoCreateUserRepository
+    );
+
+    const { body, statusCode } = await createUserController.handle({
+      body: req.body,
+    });
     res.send(body).status(statusCode);
   });
 
--- src/repositories/create-user/mongo-create-user..ts
+++ src/repositories/create-user/mongo-create-user..ts
@@ -5,7 +5,7 @@ import {
 import { MongoClient } from "../../database/mongo";
 import { IUser } from "../../models/user";
 
-export class MongoCreateUser implements ICreateUserRepository {
+export class MongoCreateUserRepository implements ICreateUserRepository {
   async createUser(params: CreateUserParams): Promise<IUser> {
     const { insertedId } = await MongoClient.db
       .collection("users")
