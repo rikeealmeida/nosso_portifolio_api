140b085f4465a754024a2cf1d8f20c7079c4ff40
unknown <henrique.almeida.ads@gmail.com>
2022-12-28 16:42:40 -0300

    feat: add MongoUpdateUserRepository

---
+++ src/controllers/update-user/protocols.ts
@@ -0,0 +1,19 @@
+import { IUser } from "../../models/user";
+import { IJob } from "../../types/job";
+export interface UpdateUserParams {
+  firstName?: string;
+  lastName?: string;
+  password?: string;
+  title?: string;
+  imageUrl?: string;
+  linkedinUrl?: string;
+  instagramUrl?: string;
+  githubUrl?: string;
+  telNumber?: string;
+  resume?: string;
+  jobs?: IJob[];
+}
+
+export interface IUpdateUserRepository {
+  updateUser(params: UpdateUserParams): Promise<IUser>;
+}
