dc8c192e7aeea719601ba770f2b7ce55f87e6de4
unknown <henrique.almeida.ads@gmail.com>
2022-12-29 14:23:04 -0300

    feat: add MongoDeleteUserRepository

---
+++ src/repositories/delete-user/mongo-delete-user.ts
@@ -0,0 +1,23 @@
+import { ObjectId } from "mongodb";
+import { IDeleteUserRepository } from "../../controllers/delete-user/protocols";
+import { MongoClient } from "../../database/mongo";
+import { IUser } from "../../models/user";
+
+export class MongoDeleteUserRepository implements IDeleteUserRepository {
+  async deleteUser(id: string): Promise<IUser> {
+    const user = await MongoClient.db
+      .collection<Omit<IUser, "id">>("users")
+      .findOne({ _id: new ObjectId(id) });
+    if (!user) {
+      throw new Error("User not found!");
+    }
+    const { deletedCount } = await MongoClient.db
+      .collection("users")
+      .deleteOne({ _id: new ObjectId(id) });
+    if (!deletedCount) {
+      throw new Error("User not deleted");
+    }
+    const { _id, ...rest } = user;
+    return { id: _id.toHexString(), ...rest };
+  }
+}
