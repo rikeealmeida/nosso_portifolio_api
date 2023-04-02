23014e3e2c05242fa0c46c61cef0398fb2b1efb1
rikeealmeida <henrique.almeida.ads@gmail.com>
2023-04-02 02:22:26 -0300

    first commit

--- src/controllers/user/user-login/user-login.ts
+++ src/controllers/user/user-login/user-login.ts
@@ -1,38 +1,38 @@
-
 import { badRequest, ok, serverError } from "../../../helpers/helpers";
 import { IUser } from "../../../interfaces/user";
 import { HttpRequest, HttpResponse, IController } from "../../protocols";
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 import { MongoClient } from "../../../database/mongo";
-import bcrypt from 'bcrypt'
+import bcrypt from "bcrypt";
 
 export class UserLoginController implements IController {
-    async handle(httpRequest: HttpRequest<IUser | undefined>): Promise<HttpResponse<string>> {
-        try {
-            const { email, password } = httpRequest.body!
+  async handle(
+    httpRequest: HttpRequest<IUser | undefined>
+  ): Promise<HttpResponse<string>> {
+    try {
+      const { email, password } = httpRequest.body!;
 
-            const user = await MongoClient.db.collection('users').findOne({ email })
+      const user = await MongoClient.db.collection("users").findOne({ email });
 
-            if (!user) {
-                return badRequest('E-mail ou senha inválidos!')
-            }
+      if (!user) {
+        return badRequest("E-mail ou senha inválidos!");
+      }
 
-            const verifyPassword = await bcrypt.compare(password, user.password)
+      const verifyPassword = await bcrypt.compare(password, user.password);
 
-            if (!verifyPassword) {
-                return badRequest('E-mail ou senha inválidos!')
-            }
+      if (!verifyPassword) {
+        return badRequest("E-mail ou senha inválidos!");
+      }
 
-            const token = jwt.sign({ id: user._id }, process.env.JWT_PASS ?? '', { expiresIn: '8h' })
+      const token = jwt.sign({ id: user._id }, process.env.JWT_PASS ?? "", {
+        expiresIn: "8h",
+      });
 
-            const { password: _, jobs: __, ...userLogin } = user
+      const { password: _, jobs: __, ...userLogin } = user;
 
-            return ok({ user: userLogin, token: token })
-
-
-        }
-        catch (error) {
-            return serverError()
-        }
+      return ok({ user: userLogin, token: token });
+    } catch (error) {
+      return serverError();
     }
+  }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,79 +1,78 @@
-import { Router } from 'express'
-import { CreateUserController } from './controllers/user/create-user/create-user';
-import { DeleteUserController } from './controllers/user/delete-user/delete-user';
-import { GetUserController } from './controllers/user/get-user/get-user';
-import { GetUsersController } from './controllers/user/get-users/get-users';
-import { UpdateUserController } from './controllers/user/update-user/update-user';
-import { UserLoginController } from './controllers/user/user-login/user-login';
-import { MongoCreateUserRepository } from './repositories/user/create-user/mongo-create-user.';
-import { MongoDeleteUserRepository } from './repositories/user/delete-user/mongo-delete-user';
-import { MongoGetUserRepository } from './repositories/user/get-user/mongo-get-user';
-import { MongoGetUsersRepository } from './repositories/user/get-users/mongo-get-users';
-import { MongoUpdateUserRepository } from './repositories/user/update-user/mongo-update-user';
+import { Router } from "express";
+import { CreateUserController } from "./controllers/user/create-user/create-user";
+import { DeleteUserController } from "./controllers/user/delete-user/delete-user";
+import { GetUserController } from "./controllers/user/get-user/get-user";
+import { GetUsersController } from "./controllers/user/get-users/get-users";
+import { UpdateUserController } from "./controllers/user/update-user/update-user";
+import { UserLoginController } from "./controllers/user/user-login/user-login";
+import { MongoCreateUserRepository } from "./repositories/user/create-user/mongo-create-user.";
+import { MongoDeleteUserRepository } from "./repositories/user/delete-user/mongo-delete-user";
+import { MongoGetUserRepository } from "./repositories/user/get-user/mongo-get-user";
+import { MongoGetUsersRepository } from "./repositories/user/get-users/mongo-get-users";
+import { MongoUpdateUserRepository } from "./repositories/user/update-user/mongo-update-user";
 
-export const routes = Router()
-
+export const routes = Router();
 
 routes.get("/users", async (req, res) => {
-    const mongoGetUsersRepository = new MongoGetUsersRepository();
-    const getUsersController = new GetUsersController(mongoGetUsersRepository);
-    const { body, statusCode } = await getUsersController.handle();
+  const mongoGetUsersRepository = new MongoGetUsersRepository();
+  const getUsersController = new GetUsersController(mongoGetUsersRepository);
+  const { body, statusCode } = await getUsersController.handle();
 
-    res.status(statusCode).send(body);
+  res.status(statusCode).send(body);
 });
 
 routes.get("/users/:id", async (req, res) => {
-    const mongoGetUserRepository = new MongoGetUserRepository()
-    const getUserController = new GetUserController(mongoGetUserRepository)
-    const { body, statusCode } = await getUserController.handle({
-        params: req.params
-    })
-    res.status(statusCode).send(body)
-})
+  const mongoGetUserRepository = new MongoGetUserRepository();
+  const getUserController = new GetUserController(mongoGetUserRepository);
+  const { body, statusCode } = await getUserController.handle({
+    params: req.params,
+  });
+  res.status(statusCode).send(body);
+});
 
 routes.post("/users", async (req, res) => {
-    const mongoCreateUserRepository = new MongoCreateUserRepository();
-    const createUserController = new CreateUserController(
-        mongoCreateUserRepository
-    );
+  const mongoCreateUserRepository = new MongoCreateUserRepository();
+  const createUserController = new CreateUserController(
+    mongoCreateUserRepository
+  );
 
-    const { body, statusCode } = await createUserController.handle({
-        body: req.body,
-    });
-    res.status(statusCode).send(body);
+  const { body, statusCode } = await createUserController.handle({
+    body: req.body,
+  });
+  res.status(statusCode).send(body);
 });
 
 routes.patch("/users/:id", async (req, res) => {
-    const mongoUpdateUserRepository = new MongoUpdateUserRepository();
-    const updateUserController = new UpdateUserController(
-        mongoUpdateUserRepository
-    );
+  const mongoUpdateUserRepository = new MongoUpdateUserRepository();
+  const updateUserController = new UpdateUserController(
+    mongoUpdateUserRepository
+  );
 
-    const { body, statusCode } = await updateUserController.handle({
-        body: req.body,
-        params: req.params,
-    });
-    res.status(statusCode).send(body);
+  const { body, statusCode } = await updateUserController.handle({
+    body: req.body,
+    params: req.params,
+  });
+  res.status(statusCode).send(body);
 });
 
 routes.delete("/users/:id", async (req, res) => {
-    const mongoDeleteUserRepository = new MongoDeleteUserRepository();
-    const deleteUserController = new DeleteUserController(
-        mongoDeleteUserRepository
-    );
+  const mongoDeleteUserRepository = new MongoDeleteUserRepository();
+  const deleteUserController = new DeleteUserController(
+    mongoDeleteUserRepository
+  );
 
-    const { body, statusCode } = await deleteUserController.handle({
-        params: req.params,
-    });
-    res.status(statusCode).send(body);
+  const { body, statusCode } = await deleteUserController.handle({
+    params: req.params,
+  });
+  res.status(statusCode).send(body);
 });
 
 routes.post("/login", async (req, res) => {
-    const loginController = new UserLoginController()
+  const loginController = new UserLoginController();
 
-    const { body, statusCode } = await loginController.handle({
-        body: req.body,
-        params: req.params
-    });
-    res.status(statusCode).send(body)
+  const { body, statusCode } = await loginController.handle({
+    body: req.body,
+    params: req.params,
+  });
+  res.status(statusCode).send(body);
 });
